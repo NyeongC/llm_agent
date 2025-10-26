0c0bdf82ca34ec4f2a20fa9f027e743dbc4851ca
NyeongC <cjfsud23@naver.com>
2025-10-26 23:28:59 +0900

    API키 python-dotenv 활용

--- venv/chap02/gpt_basic.py
+++ venv/chap02/gpt_basic.py
@@ -1,6 +1,10 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-api_key = ''	# ① 
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI(api_key=api_key)
 
